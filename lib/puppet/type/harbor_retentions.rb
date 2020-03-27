8357930b491456696974595ab39a17f47b1b7705
Martin, Dereck <Dereck.Martin@bottomline.com>
2020-03-27 09:14:54 -0400

    update

--- lib/puppet/type/harbor_retentions.rb
+++ lib/puppet/type/harbor_retentions.rb
@@ -1,17 +1,19 @@
 # frozen_string_literal: true
 
 Puppet::Type.newtype(:harbor_retentions_policy) do
-    desc <<-DESC
+  desc <<-DESC
   @summary Manage Harbor retention policies
   
   @example Creating a replication policy within Harbor
-      harbor_replication_policy { 'example-replication':
+      harbor_replication_policy { 'example-retention':
         ensure           => 'present',
-        deletion         => false,
-        enabled          => true,
-        override         => false,
         policy           => {
-            rules => [ tag_selectors   => [{ decoration => "string", pattern = "string", kind => "string" }],
+            rules => [ 
+              tag_selectors => [{ 
+                decoration => "string", 
+                pattern = "string", 
+                kind => "string" 
+              }],
             action => "string"
             trigger => {},
             scope => {},
@@ -19,23 +21,11 @@ Puppet::Type.newtype(:harbor_retentions_policy) do
       }
   DESC
   
-    ensurable
+  ensurable
   
-    newparam(:policy) do
-      desc 'The Rentention Policy'
-      newvalues(:push, :pull)
-    end
-  
-    newproperty(:deletion) do
-      desc 'Whether to replicate the deletion operation'
-    end
-  
-    newproperty(:override) do
-      desc 'Whether to override the resources on the destination registry'
-    end
-  
-    newproperty(:enabled) do
-      desc 'Whether the policy is enabled or not'
-    end
+  newparam(:policy) do
+    desc 'The Rentention Policy'
+    newvalues(:push, :pull)
   end
+end
   
